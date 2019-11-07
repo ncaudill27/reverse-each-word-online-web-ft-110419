2eb8f1ef9d4a541264b046e25dc76641c0a272be
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-07 02:17:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(phrase)
   words = phrase.split
-  reversed = words.map { |word| word.reverse }
+  reversed = words.collect { |word| word.reverse }
   final = reversed.join(" ")
 end
